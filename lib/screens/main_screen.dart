2a31e028ec241f19679b924d236a00a8dad2d5f3
이경준 <oasis405@naver.com>
2025-10-25 21:07:56 +0900

    fix: refresh bug fix

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -13,19 +13,23 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
 
-  final List<Widget> _screens = [
-    const HomeScreen(),
-    const HistoryScreen(),
-    const ManageQuestionsScreen(),
-  ];
+  Widget _getCurrentScreen() {
+    switch (_currentIndex) {
+      case 0:
+        return const HomeScreen();
+      case 1:
+        return const HistoryScreen();
+      case 2:
+        return const ManageQuestionsScreen();
+      default:
+        return const HomeScreen();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _screens,
-      ),
+      body: _getCurrentScreen(),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
